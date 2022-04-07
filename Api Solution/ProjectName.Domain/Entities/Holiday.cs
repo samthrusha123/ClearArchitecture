87d5de5d03b57495c7e7de83b91381fac055a2f1
Elsa <44087954+samthrusha123@users.noreply.github.com>
2022-04-07 12:44:18 +0530

    Add files via upload

---
+++ Api Solution/ProjectName.Domain/Entities/Holiday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectName.Domain.Entities
+{
+    public class Holiday
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Occasion { get; set; }
+    }
+}
